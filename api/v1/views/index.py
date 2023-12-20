81307483d230f36cd23bca1f8220b37eced4eb16
Erkenoss <xavier.bertin6@gmail.com>
2023-12-20 17:53:22 +0100

    fixed task 5-

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -19,12 +19,12 @@ def get_stats():
     """retrieves the number of each object"""
 
     stats = {
-        storage.count('Amenities'),
-        storage.count('City'),
-        storage.count('Place'),
-        storage.count('Review'),
-        storage.count('State'),
-        storage.count('user'),
+        "amenities": storage.count('Amenity'),
+        "cities": storage.count('City'),
+        "places": storage.count('Place'),
+        "reviews": storage.count('Review'),
+        "states": storage.count('State'),
+        "users": storage.count('User'),
     }
 
     return jsonify(stats)
