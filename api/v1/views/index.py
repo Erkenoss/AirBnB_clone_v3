2a96c3cdcb237203d1d74992e0037518d6b020b9
Erkenoss <xavier.bertin6@gmail.com>
2023-12-20 17:41:32 +0100

    fixed task 5-

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -14,6 +14,7 @@ def get_status():
     return jsonify({"status": "OK"})
 
 
+@app_views.route('/api/v1/stats', methods=['GET'], strict_slashes=False)
 def get_stats():
     """retrieves the number of each object"""
 
