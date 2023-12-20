6fc70d01cc2e1db6bf1527a9c1b19fd5335c7646
Erkenoss <xavier.bertin6@gmail.com>
2023-12-20 16:57:56 +0100

    pycode

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,6 +7,7 @@ A module that contains a route for app_views.
 from api.v1.views import app_views
 from flask import jsonify
 
+
 @app_views.route('/status', methods=['GET'])
 def get_status():
     return jsonify({"status": "OK"})
