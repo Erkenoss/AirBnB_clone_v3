86adc93a5577ed5081249a8224a5a0262b90abef
Erkenoss <xavier.bertin6@gmail.com>
2023-12-20 17:21:06 +0100

    add task 5-

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,8 +6,24 @@ A module that contains a route for app_views.
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'])
 def get_status():
     return jsonify({"status": "OK"})
+
+
+def get_stats():
+    """retrieves the number of each object"""
+
+    stats = {
+        storage.count('Amenities'),
+        storage.count('City'),
+        storage.count('Place'),
+        storage.count('Review'),
+        storage.count('State'),
+        storage.count('user'),
+    }
+
+    return jsonify(stats)
